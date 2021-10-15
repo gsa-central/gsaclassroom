cbf84e86093c699ea27efd1713e0e746274cb2f1
MugZone53 <74393557+MugZone53@users.noreply.github.com>
2021-10-15 15:03:06 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,17 @@
-
+shinyUI(fluidPage(
+    tags$div(
+      div(img(src="GSACENTRAL_Logo.png", width = "100px", height = "50px"),
+              style = "padding-left:5px;"),
+      class = "text-center",
+      span(strong(h2("GSA Classroom Videos")),
+           style = "color:steelblue")),
+    tags$hr(),
+    fluidRow(DT::dataTableOutput("DT1")),
+    tags$hr(),
+    tags$div(
+      div(img(src="GSACENTRAL_Logo.png", width = "100px", height = "50px"),
+              style = "padding-left:5px;"),
+      class = "text-center",
+      span(strong(h4("Last updated: October 15th, 2021")),
+           style = "color:gray"))
+))
