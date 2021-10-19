d9e7d5c8f72f40c5bc1e1008d148b900eecdc6da
MugZone53 <74393557+MugZone53@users.noreply.github.com>
2021-10-19 13:07:26 +0800

    Update global.R

--- global.R
+++ global.R
@@ -1,1 +1,16 @@
+library(shiny)
+library(shinythemes)
+library(DT)
+library(hms)
+library(reshape2)
+library(readr)
+library(readxl)
+
+## read videodata into R
+file = "data/videos.xlsx"
+
+## openxlsx::read.xlsx(file,1, startRow=5)
+vdata <- readxl::read_excel(file, sheet = 1, range = "A1:G31")
+
+
 
