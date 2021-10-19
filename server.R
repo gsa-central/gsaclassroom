877939467ae3fca3ddf183a7ebccd87c27ff9dc9
MugZone53 <74393557+MugZone53@users.noreply.github.com>
2021-10-19 13:07:00 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,27 @@
+shinyServer(function(input, output, session){
+  
+  options(shiny.maxRequestSize=100*1024^2)
+  
+  source("global.R")
+  data <- reactive(vdata)
+
+  output$DT1 <- DT::renderDataTable(
+    DT::datatable(data(),
+                  rownames = FALSE,
+                  selection = "none",
+                  options = list(autoFill = TRUE,
+                                 searchHighlight = TRUE,
+                                 paging = TRUE,
+                                 dom = 'Blftsip',
+                                 buttons = list(list(extend = "collection",
+                                                     buttons = list(list(extend='csv',
+                                                                         filename = 'GSA-related videos'),
+                                                                    list(extend = 'excel',
+                                                                         filename = 'GSA-related videos')),
+                                                     text = "Download")),
+                                 lengthMenu = list(c(25, 50, -1),
+                                                   c(25, 50, "All") 
+                                 )
+          )))
+})
 
